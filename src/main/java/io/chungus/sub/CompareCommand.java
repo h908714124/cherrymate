c956f8d687cb7c34efadee253ef60cbb84151eb6
jbock <jbock-java@gmx.de>
2021-09-16 11:43:51 +0200

    refactor/rename

--- src/main/java/io/chungus/sub/CompareCommand.java
+++ src/main/java/io/chungus/sub/CompareCommand.java
@@ -23,10 +23,10 @@ public class CompareCommand {
     }
 
     public void compare() {
-        List<RevCommit> sourceCommits = fromBase(this.sourceCommits);
-        Map<String, List<RevCommit>> source = groupByMessage(sourceCommits);
+        List<RevCommit> sc = fromBase(sourceCommits);
+        Map<String, List<RevCommit>> source = groupByMessage(sc);
         Map<String, List<RevCommit>> target = groupByMessage(fromBase(targetCommits));
-        List<String> messages = sourceCommits.stream().map(RevCommit::getShortMessage).distinct().toList();
+        List<String> messages = sc.stream().map(RevCommit::getShortMessage).distinct().toList();
         for (String message : messages) {
             List<RevCommit> s = source.getOrDefault(message, List.of());
             List<RevCommit> t = target.getOrDefault(message, List.of());
